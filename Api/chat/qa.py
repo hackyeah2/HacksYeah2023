0275725f8796d71ec2565fc8b0be89bcdaef90b2
gamemeine <grzegorz.statkiewicz@gmail.com>
2023-10-01 01:03:37 +0200

    Refactor

--- Api/chat/qa.py
+++ Api/chat/qa.py
@@ -1,5 +1,6 @@
 import os
 import pandas
+from data.assets.resource import Resource
 from data.assets.asset import AssetType
 from models.answer import Answer
 from models.question import Question
@@ -10,13 +11,20 @@ def answer(question: Question):
     type = AssetType.REAL_ESTATE
 
     related_resources = embedding.search(type, question.question)
+    top_resource = get_top_resource(question, related_resources)
 
-    if len(related_resources) == 0:
+    if top_resource is None:
         return Answer('No resources found', None, question.sessionId)
-
-    top_resource = related_resources[0]
 
     top_resource_absolute_path = os.path.join(os.getcwd(), top_resource.source)
     data = pandas.read_csv(top_resource_absolute_path, delimiter=";")
 
     return Answer('Found resources:', data, question.sessionId)
+
+
+def get_top_resource(question: Question, resources: [Resource]):
+    if len(resources) == 0:
+        return None
+
+    top_resource = resources[0]
+    return top_resource
